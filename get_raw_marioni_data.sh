719f47ff8c2b5d5285df066b2ddd601518063c8e
John Steill <jsteill@morgridge.org>
2020-04-27 09:09:10 -0500

    notes on downloading raw data

---
+++ get_raw_marioni_data.sh
@@ -0,0 +1,9 @@
+# https://github.com/MarioniLab/EmbryoTimecourse2018
+# https://github.com/MarioniLab/EmbryoTimecourse2018/blob/master/download/download.sh
+#
+# The download.sh script didn't work: the *.tar.gz files wouldn't uncompress
+# But the alternate method of zip files did:
+#
+# curl https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6967/E-MTAB-6967.processed.1.zip > atlas.zip &
+# unzip atlas.zip > atlas
+# tar -xvf atlas_data.tar.gz 
